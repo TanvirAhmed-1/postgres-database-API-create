9369ba337390c3fc77fbaa739e7dfe56eebc12cc
TanvirAhmed-1 <tanvirahmed1078@gmail.com>
2025-06-30 22:44:57 +0600

    product api complete

--- index.js
+++ index.js
@@ -1,69 +1,90 @@
-const express = require('express');
-const port=process.env.PORT || 5000
-const app=express()
-const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const port = process.env.PORT || 5000;
+const app = express();
+const { v4: uuidv4 } = require("uuid");
 
-const pool=require("./db")
+const pool = require("./db");
 
-app.use(express.json())
+app.use(express.json());
 
+app.get("/products", async (req, res) => {
+  try {
+    const result = await pool.query("select* from product");
+    res
+      .status(200)
+      .send({ message: `all products show`, product: result.rows });
+  } catch (error) {
+    res.send({ error: "error.message" });
+  }
+});
 
-app.get("/products",async(req,res)=>{
-    try {
-      res.status(200).send({message:`all products show`}) 
-    } catch (error) {
-       res.send({ error: "error.message" });
-    }
-})
+app.post("/products", async (req, res) => {
+  try {
+    const { name, price } = req.body;
+    const id = uuidv4();
 
-app.post("/products",async(req,res)=>{
-    try {
-       const {name, price}=req.body
-       const id=uuidv4();
+    const result = await pool.query(
+      "INSERT INTO product(id,name,price) VALUES($1,$2,$3) RETURNING*",
+      [id, name, price]
+    );
 
-       const result=await pool.query("INSERT INTO product(id,name,price) VALUES($1,$2,$3) RETURNING*",[id,name,price])
+    res
+      .status(200)
+      .send({
+        message: `the product name ${name} and ${price}`,
+        product: result.rows,
+      });
+  } catch (error) {
+    res.send({ error: "error.message" });
+  }
+});
 
-        res.status(200).send({message:`the product name ${name} and ${price}`,product:result.rows})
-    } catch (error) {
-     res.send({ error: "error.message" });  
-    }
-})
+app.get("/products/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await pool.query("select* from product where id=$1", [id]);
+    res
+      .status(200)
+      .send({ message: `the product id ${id}`, product: result.rows });
+  } catch (error) {
+    res.send({ error: "error.message" });
+  }
+});
 
+app.put("/products/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { name, price } = req.body;
+    const result = await pool.query(
+      "update product set name=$1, price=$2 where id=$3 RETURNING*",
+      [name, price, id]
+    );
+    res
+      .status(200)
+      .send({
+        message: `the product  ${name} and ${price} ${id}`,
+        product: result.rows,
+      });
+  } catch (error) {
+    res.send({ error: "error.message" });
+  }
+});
 
-app.get("/products/:id",async(req,res)=>{
-    try {
-        const id=req.params.id
-       const result=await pool.query("select* from product where id=$1",[id])
-        res.status(200).send({message:`the product id ${id}`,product:result.rows})
-    } catch (error) {
-      res.send({ error: "error.message" }); 
-    }
-})
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await pool.query("delete from product where id=$1", [id]);
+    res
+      .status(200)
+      .send({ message: `the delete product `, product: result.rows });
+  } catch (error) {
+    res.send({ error: "error.message" });
+  }
+});
 
-app.put("/products/:id",async(req,res)=>{
-    try {
-        const id=req.params.id
-          const {name, price}=req.body
-        res.status(200).send({message:`the product  ${name} and ${price} ${id}`})
-    } catch (error) {
-      res.send({ error: "error.message" }); 
-    }
-})
-
-app.delete("/products/:id",async(req,res)=>{
-    try {
-        const id=req.params.id
-
-        res.status(200).send({message:`the delete product id ${id}`})
-    } catch (error) {
-      res.send({ error: "error.message" });  
-    }
-})
-
-
-app.get("/", (req,res)=>{
-    res.send("server is Running")
-})
-app.listen(port,()=>{
-    console.log(`the server is Running port ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("server is Running");
+});
+app.listen(port, () => {
+  console.log(`the server is Running port ${port}`);
+});
